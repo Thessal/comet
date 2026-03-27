acb1ae1eac5d5944d78fdb50260bd6d42c3ab9bb
Jongkook Choi <jongkook90@localhost>
2026-03-27 07:13:38 +0000

    cuda lib linking

--- shell.nix
+++ shell.nix
@@ -31,4 +31,11 @@ in pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
     python313 python313.pkgs.numpy
   ]) ;
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  shellHook = ''
+     export CUDA_PATH=${pkgs.cudatoolkit}
+     export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidiaPackages.stable}/lib:${pkgs.cudaPackages.cuda_nvrtc.lib}/lib:$LD_LIBRARY_PATH
+     # LD_LIBRARY_PATH=/run/opengl-driver/lib
+     export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidiaPackages.stable}/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+     export EXTRA_CCFLAGS="-I/usr/include"
+  '';    
 }
