8fdbb0814639c968d5037208bad2055b3b7e3c48
Jongkook Choi <jongkook90@hotmail.com>
2026-06-02 10:47:33 +0900

    Updated README.md

--- README.md
+++ README.md
@@ -8,25 +8,40 @@ Comet is a quant algo fuzzer.
 Here is a minimal `.cm` script formulating a ratio between a raw time-series feature and its moving average:
 
 ```comet
-Behavior Comparator(signal: DataFrame, eps: Float, reference: DataFrame) {
-    weights="behavior_1_compare.pth", train=true, supervised_epochs=100
+Behavior Comparator(signal: DataFrame, reference: DataFrame) {
+    weights="behavior_1_compare.pth", train=true, supervised_epochs=100,
+    operators = [add, divide],
+    integers = [ ], floats = [ ], strings=[ ]
 } -> DataFrame
 
 Flow volume_spike {
     volume = data("volume")
     adv20 = data("adv20")
-    Comparator(volume, 0.1, adv20)
+    Comparator(volume, adv20)
 }
 ```
 
 ## Usage 
 
-Transformer pretraining
+Random Search
 ```
-cargo run -- examples/behavior_1.cm
+cargo run --release -- --file ./examples/behavior_1.cm
+
+Expr: add(data("adv20"), divide(data("adv20"), add(add(data("volume"), add(data("adv20"), add(data("adv20"), data("adv20")))), data("adv20"))))
+Expr: data("volume")
+Expr: data("adv20")
+Expr: add(data("volume"), add(divide(add(add(divide(data("adv20"), data("adv20")), divide(data("volume"), divide(data("adv20"), data("volume")))), divide(divide(data("volume"), data("adv20")), divide(data("volume"), data("adv20")))), divide(data("volume"), data("adv20"))), add(data("adv20"), data("volume"))))
+Expr: data("adv20")
+Expr: data("volume")
+Expr: data("volume")
+Expression failed to terminate
+Expr: data("volume")
+Expr: divide(divide(divide(data("adv20"), data("adv20")), data("volume")), data("adv20"))
 ```
 
 ## For non-nixos
+DGX Spark (ARM64)
+
 See: 
 https://github.com/Thessal/libtorch-releases/releases/tag/v2.9.0
 
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -19,7 +19,10 @@ struct Args {
 }
 
 fn main() {
-    let args = Args::parse();
+    _main(Args::parse());
+}
+
+fn _main(args: Args) {
     let use_cuda = args.cuda || std::env::var("CUDA_PATH").is_ok();
     let filename = &args.file;
     let src = fs::read_to_string(filename).expect("Failed to read file");
@@ -32,66 +35,8 @@ fn main() {
         NodeType::Behavior(b) => b,
         _ => unreachable!(),
     };
-    // let behavior_decls = Vec::new(); // Dummy since it was undefined
     let action_space: ActionSpace = behavior_decl.into();
-    // impl From<&BehaviorDecl> for ActionSpace {
     bruteforce::brute_force(network, root, action_space);
-
-    // // Select first train=True Behavior
-    // let mut target_behavior = None;
-    // for decl in &behavior_decls {
-    //     if let InputDecl::Behavior(b) = decl {
-    //         if Some(true) == b.train {
-    //             target_behavior = Some(b.clone());
-    //         }
-    //     }
-    // }
-    // let behavior = target_behavior.expect("No train=True behavior found");
-    // println!("--- Selected behavior ---");
-
-    // // Initialize central runtime
-    // let mut runtime = runtime::runtime::Runtime::new(10000, "data".into(), None);
-    // runtime.enable = false; // NOTE: dummy runtime
-
-    // let device = if use_cuda {
-    //     tch::Device::Cuda(0)
-    // } else {
-    //     tch::Device::Cpu
-    // };
-
-    // println!("--- Starting RL Fine-Tuning ---");
-    // let score_fn = runtime::stats::Aggregator {
-    //     weights: HashMap::from_iter([
-    //         // (runtime::stats::Metric::Sharpe, (0.5, 0., 1.)),
-    //         // (runtime::stats::Metric::Ret, (0.5, 0., 1.)),
-    //     ]),
-    // };
-
-    // let mut env = rl::env::Environment::new(
-    //     &mut runtime,
-    //     behavior.clone(),
-    //     params,
-    //     score_fn,
-    //     20,   // max_length
-    //     1024, // batch_size
-    // );
-
-    // let action_vocab_size = env.action_space.size();
-    // let config =
-    //     rl::model::ModelConfig::RnnModel(rl::model::ModelSize::Small.get_config(action_vocab_size));
-    // let vs = tch::nn::VarStore::new(device);
-    // let model = config.init(&vs.root());
-
-    // env.run(&model, 100, device);
-
-    // println!("--- Evaluation ---");
-    // for i in 0..10 {
-    //     let traj = env.sample_trajectory(&model, device);
-    //     println!("Sample {}:", i);
-    //     for step in traj {
-    //         println!("  Action: {:?}", step.action);
-    //     }
-    // }
 }
 
 #[cfg(test)]
@@ -103,17 +48,9 @@ mod tests {
     fn test_behavior_1() {
         let filename = "../examples/behavior_1.cm";
         let src = fs::read_to_string(filename).expect("Failed to read file");
-        println!("--- Parsing file: {:?} ---", filename);
-        let (network, root, behavior_nodes) =
-            parser::parser::parse(&src).expect(format!("Failed to parse {:?}", filename).as_str());
-
-        let behavior_decl: &BehaviorDecl = match &network.nodes[behavior_nodes[0]].node_type {
-            NodeType::Behavior(b) => b,
-            _ => unreachable!(),
-        };
-        // let behavior_decls = Vec::new(); // Dummy since it was undefined
-        let action_space: ActionSpace = behavior_decl.into();
-        // impl From<&BehaviorDecl> for ActionSpace {
-        bruteforce::brute_force(network, root, action_space);
+        _main(Args {
+            file: String::from(filename),
+            cuda: true,
+        });
     }
 }
